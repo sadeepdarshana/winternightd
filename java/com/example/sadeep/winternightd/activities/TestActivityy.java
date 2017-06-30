5c02481f95165774b3577354076f8102670982fc
Sadeep Darshana <darshanasadeep@gmail.com>
2017-06-30 18:44:46 +0530

    asdf

--- java/com/example/sadeep/winternightd/activities/TestActivityy.java
+++ java/com/example/sadeep/winternightd/activities/TestActivityy.java
@@ -2,14 +2,31 @@ package com.example.sadeep.winternightd.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.LinearLayout;
 
 import com.example.sadeep.winternightd.R;
+import com.example.sadeep.winternightd.attachbox.AttachItemsAdapter;
 
 public class TestActivityy extends AppCompatActivity {
+
+
+    LinearLayout test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test_activityy);
+
+        test = new LinearLayout(this);
+        setContentView(test);
+
+        GridView grid = new GridView(this);
+        grid.setAdapter(new AttachItemsAdapter(this));
+        grid.setColumnWidth(60);
+        grid.setNumColumns(GridView.AUTO_FIT);
+
+        test.addView(grid);
+
+
     }
 }
--- java/com/example/sadeep/winternightd/attachbox/AttachItemsAdapter.java
+++ java/com/example/sadeep/winternightd/attachbox/AttachItemsAdapter.java
@@ -1,31 +1,47 @@
 package com.example.sadeep.winternightd.attachbox;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.GridView;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Created by Sadeep on 6/30/2017.
  */
 
 public class AttachItemsAdapter extends BaseAdapter {
+    int count=500;
+    View[] items = new View[count];
+    public AttachItemsAdapter(Context context) {
+        for(int i=0;i<count;i++){
+            items[i]=new EditText(context);
+            items[i].setLayoutParams(new GridView.LayoutParams(200,200));
+            ((EditText)items[i]).setText(Integer.toString(i));
+        }
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return count;
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return items[position];
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return 0;//position/3;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        return items[position];
     }
 }
