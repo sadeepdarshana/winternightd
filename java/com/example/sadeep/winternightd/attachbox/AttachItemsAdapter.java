7f40d58e2403ca6d5d9cf549d98813dd6f7245b5
Sadeep Darshana <darshanasadeep@gmail.com>
2017-06-30 17:38:16 +0530

    asdf

---
+++ java/com/example/sadeep/winternightd/attachbox/AttachItemsAdapter.java
@@ -0,0 +1,31 @@
+package com.example.sadeep.winternightd.attachbox;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Sadeep on 6/30/2017.
+ */
+
+public class AttachItemsAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
