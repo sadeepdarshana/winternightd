4b51c798de0bbb642c108202b84f2c287a6834a0
Sadeep Darshana <darshanasadeep@gmail.com>
2017-07-13 01:55:12 +0530

    growing

--- java/com/example/sadeep/winternightd/catalog/CatalogDataHandler.java
+++ java/com/example/sadeep/winternightd/catalog/CatalogDataHandler.java
@@ -3,9 +3,8 @@ package com.example.sadeep.winternightd.catalog;
 import android.content.ContentValues;
 import android.database.Cursor;
 
-import com.example.sadeep.winternightd.dumping.RawFieldDataStream;
 import com.example.sadeep.winternightd.localstorage.DataConnection;
-import com.example.sadeep.winternightd.note.NoteInfo;
+import com.example.sadeep.winternightd.notebook.NotebookInfo;
 
 /**
  * Created by Sadeep on 7/13/2017.
@@ -19,12 +18,12 @@ public class CatalogDataHandler {
         return cursor;
     }
 
-    public void addNotebook(CatalogEntry entry){
+    public void addNotebook(NotebookInfo info){
 
         ContentValues values = new ContentValues();
-        values.put("created",entry.createdTime);
-        values.put("title",entry.title);
-        values.put("title",entry.title);
+        values.put("created",info.createdTime);
+        values.put("title",info.title);
+        values.put("notebookId",info.notebookUUID);
 
         DataConnection.writableDatabase().insert("catalog",null,values);
     }
--- java/com/example/sadeep/winternightd/notebook/NotebookInfo.java
+++ java/com/example/sadeep/winternightd/notebook/NotebookInfo.java
@@ -1,15 +1,15 @@
-package com.example.sadeep.winternightd.catalog;
+package com.example.sadeep.winternightd.notebook;
 
 /**
  * Created by Sadeep on 7/13/2017.
  */
 
-public class CatalogEntry {
+public class NotebookInfo {
     public String notebookUUID;
     public String title;
     public long createdTime;
 
-    public CatalogEntry(String notebookUUID, String title,long createdTime) {
+    public NotebookInfo(String notebookUUID, String title, long createdTime) {
         this.createdTime = createdTime;
         this.notebookUUID = notebookUUID;
         this.title = title;
